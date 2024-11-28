ea7c2e2206576a04e1a4c853f4261c7aa58bf971
Christ093 <202111260@fit.edu.ph>
2024-11-29 04:41:12 +0800

    Update RequireAssertRevert.sol

--- RequireAssertRevert.sol
+++ RequireAssertRevert.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+pragma solidity 0.8.28;
 
 contract MyToken {
 
@@ -18,45 +18,36 @@ contract MyToken {
     event Burn(address indexed account, uint value);
 
     // Mint function to increase total supply and update balance of the given address
-    function mint(address _address, uint _value) public {
+    function mint(address _address, uint _value) external {
         // Require that the value to mint is greater than zero
         require(_value > 0, "Mint value must be greater than zero");
+
+        uint previousTotalSupply = totalSupply;  // Store the previous total supply
         
         totalSupply += _value;  // Increase total supply
         balances[_address] += _value;  // Increase the balance of the given address
+        
+        // Use assert to check that total supply has increased correctly
+        assert(totalSupply == previousTotalSupply + _value);  // Ensure the total supply increased by the minted value
         
         // Emit the Mint event
         emit Mint(_address, _value);
     }
 
     // Burn function to reduce total supply and the balance of the given address
-    function burn(address _address, uint _value) public {
+    function burn(address _address, uint _value) external {
         // Require that the address has enough tokens to burn
         require(balances[_address] >= _value, "Insufficient balance to burn");
-        
-        // Use assert to check for an internal consistency condition
-        assert(balances[_address] >= 0);  // This should always be true, balances cannot be negative
 
+        uint previousTotalSupply = totalSupply;  // Store the previous total supply
+        
         totalSupply -= _value;  // Decrease total supply
         balances[_address] -= _value;  // Decrease the balance of the given address
         
+        // Use assert to check that total supply has decreased correctly
+        assert(totalSupply == previousTotalSupply - _value);  // Ensure the total supply decreased by the burned value
+        
         // Emit the Burn event
         emit Burn(_address, _value);
-    }
-
-    // Example function to check token balance and ensure the total supply is valid
-    function checkTotalSupplyConsistency() public view {
-        // Use assert to check that totalSupply is always non-negative
-        assert(totalSupply >= 0);  // This should always be true, as total supply can't be negative
-    }
-
-    // Example function to demonstrate revert with custom error message
-    function resetAccountBalance(address _address) public {
-        // If the address has no tokens, revert the transaction with a custom message
-        if (balances[_address] == 0) {
-            revert("Account already has zero balance");
-        }
-
-        balances[_address] = 0;  // Reset the balance to zero
     }
 }
